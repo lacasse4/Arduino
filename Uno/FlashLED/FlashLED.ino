5a580d7e8bc8478974012370903f83e4a7d2a37c
vlacasse <lacasse4@yahoo.com>
2023-03-15 07:38:29 -0400

    Libraries update

--- Uno/FlashLED/FlashLED.ino
+++ Uno/FlashLED/FlashLED.ino
@@ -4,6 +4,7 @@ void setup() {
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(13, OUTPUT);
+  pinMode(25, OUTPUT);
 }
 
 void loop() {
@@ -12,12 +13,14 @@ void loop() {
   digitalWrite(4, LOW);
   digitalWrite(5, LOW);
   digitalWrite(13, LOW);
+  digitalWrite(25, LOW);
   delay(500);
+
   digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
   digitalWrite(4, HIGH);
   digitalWrite(5, HIGH);
   digitalWrite(13, HIGH);
-
+  digitalWrite(25, HIGH);
   delay(500);
 }
--- libraries/TFT/src/utility/glcdfont.c
+++ libraries/TFT/src/utility/glcdfont.c
@@ -1,4 +1,5 @@
-#ifndef ARDUINO_ARCH_SAM
+#if !defined(ARDUINO_ARCH_SAM) && !defined(__ARDUINO_ARC__) && \
+    !defined(ARDUINO_ARCH_STM32)
 #include <avr/io.h>
 #endif
 #include <avr/pgmspace.h> 
